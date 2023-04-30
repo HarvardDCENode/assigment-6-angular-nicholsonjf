260f68915831aabdc8c5992b72b5d850f1195430
James Nicholson <jnicholson@hsph.harvard.edu>
2023-04-30 17:13:44 -0400

    Setup angular routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TripComponent } from './trip/trip.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/trips', pathMatch: 'full'},
+  { path: 'trips', component: TripsComponent }
+]
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { TripComponent } from './trip/trip.component';
     TripComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
